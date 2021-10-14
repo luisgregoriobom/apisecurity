1a201331d9353ab7a240c05ff4fa96136f929b6d
thomas-pauletti-develcode <thomas.pauletti@develcode.com.br>
2021-10-14 14:17:45 -0300

    springboot enviroment configured with H2 database

---
+++ src/test/java/br/com/develfoodspringweb/develfoodspringweb/DevelfoodSpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.develfoodspringweb.develfoodspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelfoodSpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
