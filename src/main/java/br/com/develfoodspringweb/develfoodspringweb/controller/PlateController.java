bfb6abf782950400de35a66ddf03ee1ad0981569
Luis Gregorio <luis.bom@develcode.com.br>
2021-11-01 15:03:59 -0300

    commit at work about creation api paltes.

--- src/main/java/br/com/develfoodspringweb/develfoodspringweb/controller/PlateController.java
+++ src/main/java/br/com/develfoodspringweb/develfoodspringweb/controller/PlateController.java
@@ -1,68 +1,23 @@
 package br.com.develfoodspringweb.develfoodspringweb.controller;
 
 import br.com.develfoodspringweb.develfoodspringweb.controller.dto.PlateDto;
-import br.com.develfoodspringweb.develfoodspringweb.controller.form.PlateForm;
 import br.com.develfoodspringweb.develfoodspringweb.models.Plate;
-import br.com.develfoodspringweb.develfoodspringweb.repository.PlateRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.List;
+
 
 @RestController
-@RequestMapping("plate")
+@RequestMapping("/api/restaurant")
 public class PlateController {
 
-    @Autowired
-    private PlateRepository plateRepository;
+    public List<PlateDto> list() {
+        Plate plate = new Plate("plate", "plate");
 
-    /**
-     * Function with GET method to do make a query with the name of the plate as parameter.
-     * @param namePlate
-     * @return
-     * @author: Thomas B.P.
-     */
-    @GetMapping
-    public PlateDto getPlateByName(@RequestParam String namePlate){
-        if(namePlate == null){
-            return null;
-        }
-
-        Optional<Plate> opt = plateRepository.findByName(namePlate);
-        if (!opt.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "Plate name not found");
-        }
-
-         return PlateDto.convertToPlateDto(opt.get());
-
+        return PlateDto.convert(Arrays.asList(plate, plate, plate));
     }
 
-    /**
-     * Function with POST method to register new Plate while the function create the URI route and return the head HTTP location with the URL
-     * @param plateForm
-     * @param uriBuilder
-     * @return
-     * @author: Thomas B.P.
-     */
-    @PostMapping
-    public ResponseEntity<PlateDto> register(@RequestBody @Valid PlateForm plateForm,
-                                             UriComponentsBuilder uriBuilder){
-       Plate plate = plateForm.convertToPlate(plateForm);
-       plateRepository.save(plate);
-
-        URI uri = uriBuilder.
-                path("/{id}").
-                buildAndExpand(plate.getId()).
-                toUri();
-
-       return ResponseEntity.created(uri).body(new PlateDto(plate));
-    }
 
 }
